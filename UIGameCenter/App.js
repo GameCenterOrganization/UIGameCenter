c778718ad890c1704eedda2ca099cbfd518b9d31
DilanGutierrez <138705122+DilanGOD02@users.noreply.github.com>
2025-11-14 15:02:13 -0600

    Update ap.js

--- UIGameCenter/App.js
+++ UIGameCenter/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/screens/UserScreen/Auth/AuthContext';
+import FlashMessage from 'react-native-flash-message';
 
 export default function App() {
   return (
     <AuthProvider>
       <AppNavigator />
+      <FlashMessage position="top" />
     </AuthProvider>
   );
 }
